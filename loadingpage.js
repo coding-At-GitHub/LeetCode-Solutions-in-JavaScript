2cac0896632b227a5436e912b2751b2c3e0f6d67
Rajesh Choudhary <101490518+coding-At-GitHub@users.noreply.github.com>
2024-04-20 18:12:09 +0530

    Update loadingpage.js

--- loadingpage.js
+++ loadingpage.js
@@ -1,6 +1,6 @@
 react native concept apply
 app try 
-  native practice 
+react native practice 
 payment handling by accounts and UPI
 web sockets in payments upi app like phone pay google pay
 next js for SSR web site
